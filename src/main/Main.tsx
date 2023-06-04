6b66ea8f1ebd0294da42dbc04060b3b4765ba2ed
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-04 19:43:28 -0300

    feat: add screens NavigationContainer

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import OnboadingScreen from '../screen/OnboadingScreen';
+import LoginScreen from '../screen/LoginScreen';
+import RegisterScreen from '../screen/RegisterScreen';
 import SecondScreen from '../screen/SecondScreen/SecondScreen copy';
 
 const Stack = createStackNavigator();
@@ -16,6 +18,14 @@ const Main = () => {
         <Stack.Screen
           name='OnboadingScreen'
           component={OnboadingScreen}
+        />
+        <Stack.Screen
+          name='LoginScreen'
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name='RegisterScreen'
+          component={RegisterScreen}
         />
         <Stack.Screen
           name='SecondScreen'
