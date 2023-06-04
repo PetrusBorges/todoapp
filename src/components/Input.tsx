8994c588274bd530da3c39fc404d75c74f4f45d2
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-04 19:41:49 -0300

    feat: create Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+  width: 100%;
+  padding: 12px;
+  color: #979797;
+  border: 1px solid #979797;
+  border-radius: 4px;
+  font-size: 16px;
+`;
