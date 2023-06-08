cbb3bc1e8febd733d285ede9b170d113be07117f
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-08 14:03:24 -0300

    feat: create ResetScreen

---
+++ src/screen/ResetPassword/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from '../../components/Text';
+import { Container } from './styles';
+
+const ResetPassword = () => {
+  return (
+    <Container>
+      <Text>reset</Text>
+    </Container>
+  );
+};
+
+export default ResetPassword;
