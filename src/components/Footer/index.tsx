b775db9f6b9410988fe4f16503fd8492665b9256
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-10 09:22:58 -0300

    feat: add CreateTaskModal component on Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useCallback } from 'react';
 
+import { CreateTaskModal } from '../CreateTaskModal';
 import { Text } from '../Text';
 import {
   Container,
@@ -9,6 +10,7 @@ import {
 } from './styles';
 
 export const Footer = () => {
+  const [createTaskModalVisible, setCreateTaskModalVisible] = useState<boolean>(false);
   const [homeSelected, setHomeSelected] = useState<boolean>(false);
   const [profileSelected, setProfileSelected] = useState<boolean>(false);
 
@@ -21,42 +23,51 @@ export const Footer = () => {
   }, []);
 
   return (
-    <Container>
-      <ButtonFooter
-        onPress={toggleHomeSelected}
-      >
-        <Image
-          source={homeSelected ? require('../../assets/icons/homeIconSelected.png') : require('../../assets/icons/homeIcon.png')}
-        />
-        <Text
-          color='#FFFFFF'
-          size={14}
-          weight='400'
-        >
-        Home
-        </Text>
-      </ButtonFooter>
+    <>
+      <CreateTaskModal
+        visible={createTaskModalVisible}
+        onClose={() => setCreateTaskModalVisible(false)}
+      />
 
-      <AddButton>
-        <Image
-          source={require('../../assets/icons/addIcon.png')}
-        />
-      </AddButton>
-
-      <ButtonFooter
-        onPress={toggleProfileSelected}
-      >
-        <Image
-          source={profileSelected ? require('../../assets/icons/profileIconSelected.png') : require('../../assets/icons/profileIcon.png')}
-        />
-        <Text
-          color='#FFFFFF'
-          size={14}
-          weight='400'
+      <Container>
+        <ButtonFooter
+          onPress={toggleHomeSelected}
         >
-        Profile
-        </Text>
-      </ButtonFooter>
-    </Container>
+          <Image
+            source={homeSelected ? require('../../assets/icons/homeIconSelected.png') : require('../../assets/icons/homeIcon.png')}
+          />
+          <Text
+            color='#FFFFFF'
+            size={14}
+            weight='400'
+          >
+            Home
+          </Text>
+        </ButtonFooter>
+
+        <AddButton
+          onPress={() => setCreateTaskModalVisible(true)}
+        >
+          <Image
+            source={require('../../assets/icons/addIcon.png')}
+          />
+        </AddButton>
+
+        <ButtonFooter
+          onPress={toggleProfileSelected}
+        >
+          <Image
+            source={profileSelected ? require('../../assets/icons/profileIconSelected.png') : require('../../assets/icons/profileIcon.png')}
+          />
+          <Text
+            color='#FFFFFF'
+            size={14}
+            weight='400'
+          >
+            Profile
+          </Text>
+        </ButtonFooter>
+      </Container>
+    </>
   );
 };
