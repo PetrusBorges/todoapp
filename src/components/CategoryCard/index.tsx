f40bc7438b203a2713fd7b60eb652ad4e78f0697
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-12 19:19:17 -0300

    feat: add hook useGetCategoryImage on CategoryCard

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
-
-import { Category } from '../../@types/Category';
+import { useGetCategoryImage } from '../../hooks/useGetCategoryImage';
 
 import { Text } from '../Text';
 import { Container, Image } from './styles';
+
+import { Category } from '../../@types/Category';
 
 interface CategoryCardProps {
   category: Category;
@@ -14,30 +15,7 @@ export const CategoryCard: FC<CategoryCardProps> = ({
   category,
   onPress,
 }) => {
-  const getCategoryImage = (categoryName: string) => {
-    switch (categoryName) {
-    case 'Grocery':
-      return require('../../assets/icons/category/breadIcon.png');
-    case 'Work':
-      return require('../../assets/icons/category/briefcaseIcon.png');
-    case 'Design':
-      return require('../../assets/icons/category/designIcon.png');
-    case 'Health':
-      return require('../../assets/icons/category/heartbeatIcon.png');
-    case 'Home':
-      return require('../../assets/icons/category/homeIcon.png');
-    case 'Social':
-      return require('../../assets/icons/category/megaphoneIcon.png');
-    case 'Music':
-      return require('../../assets/icons/category/musicIcon.png');
-    case 'Sport':
-      return require('../../assets/icons/category/sportIcon.png');
-    case 'Study':
-      return require('../../assets/icons/category/universityIcon.png');
-    case 'Movie':
-      return require('../../assets/icons/category/videoCameraIcon.png');
-    }
-  };
+  const { getCategoryImage } = useGetCategoryImage();
 
   return (
     <Container
