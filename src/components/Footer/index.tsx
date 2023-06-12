935d204b7a85c36fc8ac5795157d445159a0027e
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-12 19:58:35 -0300

    fix: fetchCategory prop on Footer and add useEffect to change selected route

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,8 @@
-import { FC, useState, useCallback } from 'react';
+import { FC, useState, useEffect } from 'react';
+
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../main/Main';
 
 import { CreateTaskModal } from '../CreateTaskModal';
 import { Text } from '../Text';
@@ -10,8 +14,10 @@ import {
 } from './styles';
 
 interface FooterProps {
-  fetchTasks: () => void;
+  fetchTasks?: () => void;
 }
+
+type FooterNavigationProps = StackNavigationProp<RootStackParamList>
 
 export const Footer: FC<FooterProps> = ({
   fetchTasks
@@ -20,13 +26,20 @@ export const Footer: FC<FooterProps> = ({
   const [homeSelected, setHomeSelected] = useState<boolean>(false);
   const [profileSelected, setProfileSelected] = useState<boolean>(false);
 
-  const toggleHomeSelected = useCallback(() => {
-    setHomeSelected((prevState) => !prevState);
-  }, []);
+  const navigation = useNavigation<FooterNavigationProps>();
+  const route = useRoute();
 
-  const toggleProfileSelected = useCallback(() => {
-    setProfileSelected((prevState) => !prevState);
-  }, []);
+  useEffect(() => {
+    const routeName = route.name;
+
+    if (routeName === 'HomeScreen') {
+      setHomeSelected(true);
+      setProfileSelected(false);
+    } else if (routeName === 'ProfileScreen') {
+      setHomeSelected(false);
+      setProfileSelected(true);
+    }
+  }, [route]);
 
   return (
     <>
@@ -38,7 +51,7 @@ export const Footer: FC<FooterProps> = ({
 
       <Container>
         <ButtonFooter
-          onPress={toggleHomeSelected}
+          onPress={() => navigation.navigate('HomeScreen')}
         >
           <Image
             source={homeSelected ? require('../../assets/icons/homeIconSelected.png') : require('../../assets/icons/homeIcon.png')}
@@ -61,7 +74,7 @@ export const Footer: FC<FooterProps> = ({
         </AddButton>
 
         <ButtonFooter
-          onPress={toggleProfileSelected}
+          onPress={() => navigation.navigate('ProfileScreen')}
         >
           <Image
             source={profileSelected ? require('../../assets/icons/profileIconSelected.png') : require('../../assets/icons/profileIcon.png')}
