10ddc09494a46babda6e4532edfb8dd95843adca
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-12 19:19:56 -0300

    feat: add hook useGetCategoryImage on EditTaskModal

--- src/components/EditTaskModal/index.tsx
+++ src/components/EditTaskModal/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useState, useEffect, useCallback } from 'react';
 import useAuth from '../../hooks/useAuth';
 import { api } from '../../services/api';
+import { useGetCategoryImage } from '../../hooks/useGetCategoryImage';
 
 import { Text } from '../../components/Text';
 import { Input } from '../../components/Input';
@@ -49,31 +50,7 @@ export const EditTaskModal: FC<EditTaskModalProps> = ({
   const [isLoadingDelete, setIsLoadingDelete] = useState<boolean>(false);
 
   const { user } = useAuth();
-
-  const getCategoryImage = (categoryName: string) => {
-    switch (categoryName) {
-    case 'Grocery':
-      return require('../../assets/icons/category/breadIcon.png');
-    case 'Work':
-      return require('../../assets/icons/category/briefcaseIcon.png');
-    case 'Design':
-      return require('../../assets/icons/category/designIcon.png');
-    case 'Health':
-      return require('../../assets/icons/category/heartbeatIcon.png');
-    case 'Home':
-      return require('../../assets/icons/category/homeIcon.png');
-    case 'Social':
-      return require('../../assets/icons/category/megaphoneIcon.png');
-    case 'Music':
-      return require('../../assets/icons/category/musicIcon.png');
-    case 'Sport':
-      return require('../../assets/icons/category/sportIcon.png');
-    case 'Study':
-      return require('../../assets/icons/category/universityIcon.png');
-    case 'Movie':
-      return require('../../assets/icons/category/videoCameraIcon.png');
-    }
-  };
+  const { getCategoryImage } = useGetCategoryImage();
 
   const toggleInput = useCallback(() => {
     if (dificultView || categoryView) {
