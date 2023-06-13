635dd542b086d8e202265b7867360f5e4bed1105
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-13 19:18:37 -0300

    fix: types User

--- src/@types/User/index.ts
+++ src/@types/User/index.ts
@@ -3,4 +3,11 @@ export interface User {
   email: string;
   password: string;
   confirmPassword: string;
+  tasks: {
+    _id: string;
+    isCompleted: boolean,
+    taskTitle: string,
+    categoryValue: string,
+    dificultValue: string
+  }[]
 }
