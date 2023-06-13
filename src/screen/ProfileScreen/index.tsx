9068762d77f91a4aeee9a89adb187f0672431e01
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-13 19:18:58 -0300

    feat: create ProfileScreen

--- src/screen/ProfileScreen/index.tsx
+++ src/screen/ProfileScreen/index.tsx
@@ -1,20 +1,149 @@
-import { FC } from 'react';
+import { useState, useCallback } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 import { Text } from '../../components/Text';
+import { Input } from '../../components/Input';
 import { Footer } from '../../components/Footer';
 import {
-  Container
+  Container,
+  Content,
+  HeaderProfile,
+  IconView,
+  TaskCountCard,
+  SettingsContainer,
+  SettingsContent,
+  SetingsAlign,
+  Image,
+  ButtonLoggout
 } from './styles';
 
-interface ProfileScreenProps {
+const ProfileScreen = () => {
+  const [inputEmailView, setInputEmailView] = useState<boolean>(false);
+  const [inputPasswordView, setInputPasswordView] = useState<boolean>(false);
 
-}
+  const { user, logout } = useAuth();
 
-const ProfileScreen: FC<ProfileScreenProps> = ({}) => {
+  const toggleEmailView = useCallback(() => {
+    if (inputPasswordView) {
+      setInputPasswordView(false);
+    }
+
+    setInputEmailView((prevState) => !prevState);
+  }, [inputPasswordView]);
+
+  const togglePasswordView = useCallback(() => {
+    if (inputEmailView) {
+      setInputEmailView(false);
+    }
+
+    setInputPasswordView((prevState) => !prevState);
+  }, [inputEmailView]);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <Container>
-      <Text color='#FFFFFF'>profile</Text>
+      <Content>
+        <HeaderProfile>
+          <Text color='#FFFFFF'>Profile</Text>
 
+          <IconView>
+            <Text
+              color='#FFFFFF'
+              size={26}
+              weight='700'
+            >{user.email.toUpperCase().charAt(0)}</Text>
+          </IconView>
+
+          <Text color='#FFFFFF'>{user.email}</Text>
+
+          <TaskCountCard>
+            <Text color='#FFFFFF'>
+              {user.tasks.length} Task left
+            </Text>
+          </TaskCountCard>
+        </HeaderProfile>
+
+        <SettingsContainer>
+          <Text color='#FFFFFF'>Account</Text>
+
+          <SettingsContent
+            onPress={toggleEmailView}
+          >
+            <SetingsAlign>
+              <Image
+                source={require('../../assets/icons/profileIcon.png')}
+              />
+
+              <Text color='#FFFFFF'>Change account email</Text>
+            </SetingsAlign>
+
+            <Image
+              style={{ transform: [{ rotate: '-90deg' }]}}
+              source={require('../../assets/icons/arrowDownIcon.png')}
+            />
+          </SettingsContent>
+
+          {inputEmailView && (
+            <Input
+              placeholder='Enter your email'
+              placeholderTextColor='#979797'
+            />
+          )}
+
+          <SettingsContent
+            onPress={togglePasswordView}
+          >
+            <SetingsAlign>
+              <Image
+                source={require('../../assets/icons/keyIcon.png')}
+              />
+
+              <Text color='#FFFFFF'>Change account password</Text>
+            </SetingsAlign>
+
+            <Image
+              style={{ transform: [{ rotate: '-90deg' }]}}
+              source={require('../../assets/icons/arrowDownIcon.png')}
+            />
+          </SettingsContent>
+
+          {inputPasswordView && (
+            <Input
+              placeholder='Enter your password'
+              placeholderTextColor='#979797'
+              secureTextEntry
+              maxLength={8}
+            />
+          )}
+
+          <SettingsContent>
+            <SetingsAlign>
+              <Image
+                source={require('../../assets/icons/cameraIcon.png')}
+              />
+
+              <Text color='#FFFFFF'>Change account image</Text>
+            </SetingsAlign>
+
+            <Image
+              style={{ transform: [{ rotate: '-90deg' }]}}
+              source={require('../../assets/icons/arrowDownIcon.png')}
+            />
+          </SettingsContent>
+
+          <ButtonLoggout
+            onPress={logout}
+          >
+            <Image
+              source={require('../../assets/icons/logoutIcon.png')}
+            />
+            <Text color='#FF4949'>Log out</Text>
+          </ButtonLoggout>
+        </SettingsContainer>
+      </Content>
       <Footer />
     </Container>
   );
--- src/screen/ProfileScreen/styles.ts
+++ src/screen/ProfileScreen/styles.ts
@@ -7,7 +7,70 @@ const isAndroid = Platform.OS === 'android';
 export const Container = styled.KeyboardAvoidingView`
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0px'};
   flex: 1;
-  align-items: center;
+  align-items: flex-start;
   justify-content: space-between;
   background-color: #121212;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const HeaderProfile = styled.View`
+  margin-top: 20px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const IconView = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 56px;
+  height: 56px;
+  border-radius: 50px;
+  background: #252626;
+`;
+
+export const TaskCountCard = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding: 17px 25px;
+  background-color: #363636;
+`;
+
+export const SettingsContainer = styled.View`
+  width: 100%;
+  margin-top: 20px;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 20px;
+  padding: 0px 20px;
+`;
+
+export const SettingsContent = styled.TouchableOpacity`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const SetingsAlign = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const Image = styled.Image``;
+
+export const ButtonLoggout = styled.TouchableOpacity`
+  margin-top: 35px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 `;
